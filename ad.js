34d8116feef3ce86427d1a39c27e95b131906bec
AnIntendedError <151703093+AnIntendedError@users.noreply.github.com>
2025-11-28 14:24:37 +1100

    Add files via upload

---
+++ ad.js
@@ -0,0 +1,6 @@
+const data = [
+    {"title": "Food Grade Double Sided Pure Titanium & Bamboo Chopping Board", "price": "25.99", "image": "https://m.media-amazon.com/images/I/71+6Q-p1KcL._AC_SX679_.jpg", "link": "https://google.com"},
+    {"title": "JBL Charge 6", "price": "109.99", "image": "https://m.media-amazon.com/images/I/8179N0kJ2QL._AC_SX466_.jpg", "link": "https://example.com"},
+    {"title": "Typhur Fast Nugget Ice Maker", "price": "199.00", "image": "https://m.media-amazon.com/images/I/61LmiC5KulL._AC_SX342_SY445_QL70_FMwebp_.jpg", "link": "https://github.com"},
+    {"title": "6GHz Gaming WiFi Router", "price": "125.99", "image": "https://m.media-amazon.com/images/I/51FvsWw1oGL._AC_SX466_.jpg", "link": "https://apple.com"}
+]
